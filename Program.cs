2ddb3651b6d2b7662a43ec14dd57d31854cc4430
Rode Gonzalez <rode.gonzalez@gmail.com>
2024-07-20 19:15:12 +0100

    CORS

--- Program.cs
+++ Program.cs
@@ -15,6 +15,17 @@ builder.Services.AddSwaggerGen(c =>
 	c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", // Nombre de la política
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200") // Especifica los orígenes permitidos
+                   .AllowAnyMethod() // Permite cualquier método (GET, POST, etc.)
+                   .AllowAnyHeader(); // Permite cualquier encabezado
+        });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -22,6 +33,8 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 } 
+
+app.UseCors("AllowSpecificOrigin"); // Usa la política de CORS definida anteriormente
 
 //builder.Services.AddScoped<IItemRepository, ItemRepository>(); // Registra el servicio de ItemsRepository
 app.MapAllEndpoints(); // Registra los endpoints de ItemsController
