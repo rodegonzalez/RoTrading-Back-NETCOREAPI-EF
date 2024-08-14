733537088585a4a2107d86b60ebfa55d9a0d17a5
Rode Gonzalez <rode.gonzalez@gmail.com>
2024-08-14 20:47:31 +0100

    Update sec and update blocksec if not exists - Ok

--- Controllers/PositionsController.cs
+++ Controllers/PositionsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using GeneralStore.Models;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GeneralStore.Controllers
 {
@@ -25,10 +26,41 @@ namespace GeneralStore.Controllers
             app.MapPost("/api/position", async (Db db, Position record) =>
             {
                 Console.WriteLine("Record: " + record);
+                string _now = DateTime.Now.ToString("yyyyMMdd HH:mm:ss");
 
-                record.Modification = DateTime.Now.ToString("yyyyMMdd HH:mm:ss");
+                record.Modification = _now;
                 await db.Positions.AddAsync(record);
                 await db.SaveChangesAsync();
+
+                // create tppblock if not exists
+                Tppblock? tppblock = await db.TppBlocks
+                                            .Where(a => a.Tppid == record.Tppid && a.Tppblocksec == record.Tppblocksec)
+                                            .FirstOrDefaultAsync();
+
+                if (tppblock is null)
+                {
+                    Tppblock newtppblock = new Tppblock();
+                    newtppblock.Tppid = record.Tppid;
+                    newtppblock.Tppblocksec = record.Tppblocksec;
+                    newtppblock.Creation = _now;
+                    newtppblock.Modification = _now;
+;                   await db.TppBlocks.AddAsync(newtppblock);
+                }
+
+                // update block secuence
+                Tppblocksecuence tppblocksecuence = new Tppblocksecuence();
+                tppblocksecuence.Positionid = record.Id;
+                tppblocksecuence.Tppid = record.Tppid;
+                tppblocksecuence.Tppblocksec = record.Tppblocksec;
+                tppblocksecuence.Sec = record.Sec;
+                tppblocksecuence.Creation = _now;
+                tppblocksecuence.Modification = _now;
+                await db.TppBlockSecuences.AddAsync(tppblocksecuence);
+
+                // update db
+                await db.SaveChangesAsync();
+
+                // return post 
                 return Results.Created($"/record/{record.Id}", record);
             });
             app.MapPut("/api/position/{id}", async (Db db, Position updaterecord, int id) =>
