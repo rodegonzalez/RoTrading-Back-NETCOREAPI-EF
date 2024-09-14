2d342ee847a21a6a58af5d998df9352d20f85bd1
Rode Gonzalez <rode.gonzalez@gmail.com>
2024-09-14 01:22:15 +0100

    Services to another file ServiceExtensions.cs

--- Program.cs
+++ Program.cs
@@ -1,51 +1,21 @@
-using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
-using GeneralStore.Models;
 using GeneralStore.Controllers;
-using GeneralStore.Interfaces;
-using BK_NetAPI_SQLite.Services;
-using BK_NetAPI_SQLite.Interfaces;
-using BK_NetAPI_SQLite.Repositories;
+using BK_NetAPI_SQLite.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-//var connectionString = builder.Configuration.GetConnectionString("rotrading") ?? "Data Source=../rotrading.db";
-//builder.Services.AddSqlite<Db>(connectionString);
 
-builder.Services.AddDbContext<Db>(options => options.UseSqlite("Data Source=../rotrading.db"));
-builder.Services.AddScoped<IPosition, PositionRepository>();
-builder.Services.AddScoped<PositionService>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-	c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin_localhost", // Nombre de la política
-        builder =>
-        {
-            builder.SetIsOriginAllowed(origin => {
-                    var uri = new Uri(origin);
-                    return uri.Host.StartsWith("192.168.0.") || uri.Host == "localhost";
-                   })
-                   .AllowAnyMethod() // Permite cualquier método (GET, POST, etc.)
-                   .AllowAnyHeader(); // Permite cualquier encabezado
-        });
-});
+// Call ConfigureServices method from ServiceExtensions.cs
+builder.Services.ConfigureServices();
 
 var app = builder.Build();
 
+// dev environment
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 } 
 
-app.UseCors("AllowSpecificOrigin_localhost"); // Usa la política de CORS definida anteriormente
-
-//builder.Services.AddScoped<IItemRepository, ItemRepository>(); // Registra el servicio de ItemsRepository
-app.MapAllEndpoints(); // Registra los endpoints de ItemsController
-app.Urls.Add("http://localhost:5100"); // Inicia en puerto 5000
+app.UseCors("AllowSpecificOrigin_localhost"); // CORS policy to use
+app.MapAllEndpoints(); // API Endpoints
+app.Urls.Add("http://localhost:5100"); // Start on port 5000
 app.Run();
---
+++ Services/ServiceExtensions.cs
@@ -0,0 +1,49 @@
+﻿using BK_NetAPI_SQLite.Interfaces;
+using BK_NetAPI_SQLite.Repositories;
+using BK_NetAPI_SQLite.Services;
+using GeneralStore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace BK_NetAPI_SQLite.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            // Agrega aquí todas las configuraciones de servicios
+
+            // Db
+            //var connectionString = builder.Configuration.GetConnectionString("rotrading") ?? "Data Source=../rotrading.db";
+            //builder.Services.AddSqlite<Db>(connectionString);
+            services.AddDbContext<Db>(options => options.UseSqlite("Data Source=../rotrading.db"));
+
+            // Repositories
+            services.AddScoped<IPosition, PositionRepository>();
+            services.AddScoped<PositionService>();
+
+            // Swagger
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            // Cors
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin_localhost", // Policy name
+                    builder =>
+                    {
+                        builder.SetIsOriginAllowed(origin => {
+                            var uri = new Uri(origin);
+                            return uri.Host.StartsWith("192.168.0.") || uri.Host == "localhost";
+                        })
+                               .AllowAnyMethod() // All methods (GET, POST, etc.)
+                               .AllowAnyHeader(); // All headers
+                    });
+            });
+
+        }
+    }
+}
